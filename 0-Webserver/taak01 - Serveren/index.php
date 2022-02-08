70cb3d0de0959bea6c37fa79827f6278c3041072
Ibrahim Guennoun <2162719@talnet.nl>
2022-02-08 10:45:21 +0100

    gettype 5x

--- 0-Webserver/taak01 - Serveren/index.php
+++ 0-Webserver/taak01 - Serveren/index.php
@@ -1,4 +1,12 @@
 <?php
-$voornaam = "tom";
-echo gettype($voornaam);
+$a = "Jan";
+$b = 2;
+$c = 2.5;
+$d = true;
+$e = null;
 
+echo gettype($a);
+echo gettype($b);
+echo gettype($c);
+echo gettype($d);
+echo gettype($e);
