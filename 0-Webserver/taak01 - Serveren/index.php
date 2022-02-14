5a182d09314a79ebabc21e15f4ab657c0268706c
Ibrahim Guennoun <2162719@talnet.nl>
2022-02-14 21:54:33 +0100

    mymovies

--- 0-Webserver/taak01 - Serveren/index.php
+++ 0-Webserver/taak01 - Serveren/index.php
@@ -1,6 +1,8 @@
 <?php
-  $item = "abc";
-  var_dump($item);
+ $myMovies = array("Spiderman", "Batman", "The Joker");
 
+echo "<li>$myMovies[0]</li>";
+echo "<li>$myMovies[1]</li>";
+echo "<li>$myMovies[2]</li>";
 
 
