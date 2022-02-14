5a34313fd5e67e4e46409760374610af3ffe323c
Ibrahim Guennoun <2162719@talnet.nl>
2022-02-14 23:13:30 +0100

    heroes

--- 0-Webserver/taak01 - Serveren/index.php
+++ 0-Webserver/taak01 - Serveren/index.php
@@ -1,9 +1,5 @@
 <?php
-$MyDognames = ['Banjer', 'Box', 'Charlie'];
-echo $MyDognames[0];
-echo $MyDognames[1];
-echo $MyDognames[2];
-$MyDognames[1] = 'Boefje';
-echo $MyDognames[0];
-echo $MyDognames[1];
-echo $MyDognames[2];
+$MyHeroes = ['Superman', 'Spiderman', 'Batman'];
+$MyHeroes[3] = 'Ironman';
+array_push($MyHeroes, 'Capman');
+print_r($MyHeroes);
