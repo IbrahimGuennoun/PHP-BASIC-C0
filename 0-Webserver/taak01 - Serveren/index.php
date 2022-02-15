78b06e0afb04c25dc8251e3c0916c24a559b3507
Ibrahim Guennoun <2162719@talnet.nl>
2022-02-15 10:24:22 +0100

    if else

--- 0-Webserver/taak01 - Serveren/index.php
+++ 0-Webserver/taak01 - Serveren/index.php
@@ -1,31 +1,51 @@
 <?php
-$test1 = 10;
+$test1 = 3;
 $test2 = 6;
 if($test1 == $test2){
 echo "<li>gelijk</li>";
+}
+else{
+    echo "<li>niet gelijk</li>";
 }
 
 if($test1 != $test2) {
     echo "<li>ongelijk</li>";
 }
+else{
+    echo "<li>gelijk</li>";
+}
 
 if($test1 === $test2){
 echo "<li>identiek</li>";
+}
+else{
+    echo "<li>niet identiek</li>";
 }
 
 if($test1 > $test2){
 echo "<li>test1 is groter dan test2</li>";
 }
+else{
+    echo "<li>test1 is niet groter dan test2</li>";
+}
 
 if($test1 < $test2){
 echo "<li>test1 is kleiner dan test2</li>";
+}
+else{
+    echo "<li>test1 is niet kleiner dan test2</li>";
 }
 
 if($test1 >= $test2){
 echo "<li>test1 is groter of gelijk aan test2</li>";
 }
-
+else{
+    echo "<li>test1 is niet groter of gelijk aan test2</li>";
+}
 if($test1 <= $test2){
 echo "<li>test1 is kleiner of gelijk aan test2</li>";
+}
+else{
+    echo "<li>test1 is niet kleiner of gelijk aan test2</li>";
 }
 
