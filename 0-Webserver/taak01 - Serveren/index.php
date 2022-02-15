82e10cb5e9bdaa1df657be737b8b8d2cbb46d89b
Ibrahim Guennoun <2162719@talnet.nl>
2022-02-15 10:40:27 +0100

    prod besteld

--- 0-Webserver/taak01 - Serveren/index.php
+++ 0-Webserver/taak01 - Serveren/index.php
@@ -1,51 +1,12 @@
 <?php
-$test1 = 3;
-$test2 = 6;
-if($test1 == $test2){
-echo "<li>gelijk</li>";
-}
-else{
-    echo "<li>niet gelijk</li>";
-}
+$prod_besteld = 12;
 
-if($test1 != $test2) {
-    echo "<li>ongelijk</li>";
+if($prod_besteld < 10){
+    echo "€1.50";
 }
-else{
-    echo "<li>gelijk</li>";
+elseif($prod_besteld < 20){
+    echo "€1.25";
 }
-
-if($test1 === $test2){
-echo "<li>identiek</li>";
+else($prod_besteld > 20){
+    echo "€1.00";
 }
-else{
-    echo "<li>niet identiek</li>";
-}
-
-if($test1 > $test2){
-echo "<li>test1 is groter dan test2</li>";
-}
-else{
-    echo "<li>test1 is niet groter dan test2</li>";
-}
-
-if($test1 < $test2){
-echo "<li>test1 is kleiner dan test2</li>";
-}
-else{
-    echo "<li>test1 is niet kleiner dan test2</li>";
-}
-
-if($test1 >= $test2){
-echo "<li>test1 is groter of gelijk aan test2</li>";
-}
-else{
-    echo "<li>test1 is niet groter of gelijk aan test2</li>";
-}
-if($test1 <= $test2){
-echo "<li>test1 is kleiner of gelijk aan test2</li>";
-}
-else{
-    echo "<li>test1 is niet kleiner of gelijk aan test2</li>";
-}
-
