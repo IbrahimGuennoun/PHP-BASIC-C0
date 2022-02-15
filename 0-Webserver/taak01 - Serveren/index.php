2d285c7c8c0d54f054ec547454de5f39e90cdda2
Ibrahim Guennoun <2162719@talnet.nl>
2022-02-15 12:45:35 +0100

    switch case

--- 0-Webserver/taak01 - Serveren/index.php
+++ 0-Webserver/taak01 - Serveren/index.php
@@ -1,12 +1,18 @@
 <?php
-$prod_besteld = 12;
-
-if($prod_besteld < 10){
-    echo "€1.50";
-}
-elseif($prod_besteld < 20){
-    echo "€1.25";
-}
-else($prod_besteld > 20){
-    echo "€1.00";
+$score = 10;
+switch($score){
+    case 10:
+        echo "Wow, wat een fantantische score!";
+        break;
+        case 9: 
+            echo "Wow bijna alle punten, nog een te gaan!";
+            case 8:
+                echo "Goed bezig, bijna alle punten";
+                break;
+                case 7:
+                    echo "Ga zo door!";
+                    break;
+                    case 6: 
+                        echo "Nog even oefenen dan haal je vast alle punten!";
+                        break;
 }
