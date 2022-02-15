0fef51b5ee20f0915bcdfafecd870f1cdc74827d
Ibrahim Guennoun <2162719@talnet.nl>
2022-02-15 09:42:01 +0100

    getal optellen

--- 0-Webserver/taak01 - Serveren/index.php
+++ 0-Webserver/taak01 - Serveren/index.php
@@ -1,8 +1,8 @@
 <?php
-$getallen  = [676,5345,78657,3,46,21,47];
-asort($getallen);
-print_r($getallen);
+$getal1 = 10;
+$getal2 = 5;
 
-$getallen  = [676,5345,78657,3,46,21,47];
-arsort($getallen);
-print_r($getallen);
+echo $getal1 * $getal2;
+echo $getal1 - $getal2;
+echo $getal1 + $getal2;
+echo $getal1 / $getal2;
