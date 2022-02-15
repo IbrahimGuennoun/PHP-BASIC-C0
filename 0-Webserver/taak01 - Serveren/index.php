45ae41d7e3d8fc03f931d0cf2eb8c44df496b289
Ibrahim Guennoun <2162719@talnet.nl>
2022-02-15 09:59:50 +0100

    var dump ==

--- 0-Webserver/taak01 - Serveren/index.php
+++ 0-Webserver/taak01 - Serveren/index.php
@@ -1,5 +1,11 @@
 <?php
-$getal1 = 10;
-echo ++$getal1;
-echo " en ";
-echo --$getal1;
+$test1 = 12;
+$test2 = "gewoon tekst";
+var_dump($test1 == $test2);
+var_dump($test1 === $test2);
+var_dump($test1 != $test2);
+var_dump($test1 > $test2);
+var_dump($test1 < $test2);
+var_dump($test1 <> $test2);
+var_dump($test1 >= $test2);
+var_dump($test1 <= $test2);
