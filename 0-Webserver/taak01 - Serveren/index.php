533fca5afc08ea07c61eb14c2e1527f168f502f2
Ibrahim Guennoun <2162719@talnet.nl>
2022-03-03 10:29:14 +0100

    leeftijd div

--- 0-Webserver/taak01 - Serveren/index.php
+++ 0-Webserver/taak01 - Serveren/index.php
@@ -1,36 +1,20 @@
-<?php
-$prod_besteld = 12;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="isMeerderJarig">
+    
+    <?php
+    
+    $leeftijd = 19;
+    if($leeftijd >18){
+        echo "Je bent meerderjarig en mag deze pagina dus bekijken";
+    }
 
-if($prod_besteld < 10) {
-    $prijs = 1.50;
-} elseif($prod_besteld < 20) {
-    $prijs = 1.25;
-} else {
-    $prijs = 1.00;
-}
-
-echo $prijs;
-
-$prod_besteld = 8;
-
-if($prod_besteld < 10) {
-    $prijs = 1.50;
-} elseif($prod_besteld < 20) {
-    $prijs = 1.25;
-} else {
-    $prijs = 1.00;
-}
-
-echo $prijs;
-
-$prod_besteld = ;
-
-if($prod_besteld < 10) {
-    $prijs = 1.50;
-} elseif($prod_besteld < 20) {
-    $prijs = 1.25;
-} else {
-    $prijs = 1.00;
-}
-
-echo $prijs;
+    ?>
+    </html>
